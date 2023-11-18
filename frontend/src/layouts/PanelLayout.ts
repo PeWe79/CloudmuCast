c2bc568dbf46f7b01b5c144feb4118fc3f689ec5
PeWe79 <poer.one79@gmail.com>
2023-11-18 15:01:28 +0700

    Remove redundant player store init.

--- frontend/src/layouts/PanelLayout.ts
+++ frontend/src/layouts/PanelLayout.ts
@@ -1,15 +1,12 @@
 import {useAzuraCast} from "~/vendor/azuracast";
 import {Component, h} from "vue";
 import PanelLayoutComponent from "~/components/PanelLayout.vue";
-import {useProvidePlayerStore} from "~/functions/usePlayerStore.ts";
 
 export default function usePanelLayout(component: Component) {
     return {
         setup() {
             const {panelProps, componentProps} = useAzuraCast();
 
-            useProvidePlayerStore('global');
-            
             return {
                 panelProps,
                 componentProps
